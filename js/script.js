42d67c9f5086a83368a386ade51f4eedb866643a
Kyle Canamar <canamar.kyle@gmail.com>
2022-03-23 21:46:06 -0700

    adds api to dnd5e, successful call

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const API ="https://www.dnd5eapi.co/api/"
+
+$.ajax(`${API}/monsters`)
+.then((data) => {
+    console.log(data)
+})
+
+$.ajax(`
+${API}monsters/adult-blue-dragon
+`)
+.then((data) => {
+    console.log(data);
+})
