2a7325313a0bcf06fd03c63a65b88903ad5c5267
Kyle Canamar <canamar.kyle@gmail.com>
2022-03-23 22:30:02 -0700

    adds search function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-const API ="https://www.dnd5eapi.co/api/"
+const API ="https://www.dnd5eapi.co/api/monsters/"
+const $input = $('input')
+const $button = $('button')
 
-$.ajax(`${API}/monsters`)
-.then((data) => {
-    console.log(data)
-})
+$button.on("click", () =>{
+    let $monsterName = $input.val()
+    $monsterName = $monsterName.replaceAll(" ","-").toLowerCase()
+    console.log($monsterName)
+    // add the input as a string formatted to ("word-word-word")
+    $.ajax(`${API}/${$monsterName}`)
+    .then((data) => { 
+        console.log("you clicked", data)
+    })
 
-$.ajax(`
-${API}monsters/adult-blue-dragon
-`)
-.then((data) => {
-    console.log(data);
 })
